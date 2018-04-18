978a1b63af161661ea35c36c507f7d41073d0b74
Stephen <859005086@qq.com>
2018-04-18 14:15:09 -0700

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { SearchService } from './search.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  results=true;
+  constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        public searchService: SearchService
+        ) { }
+  toSearchResults(){
+      
+      if(this.searchService.searchResultsAvailable){
+          this.router.navigate(['/search-results']);
+          this.searchService.buttonOfresultandfavorate=true;
+      }
+
+  }
+  toFarorate(){
+      this.searchService.buttonOfresultandfavorate=false;
+      this.router.navigate(['/favorate-results']);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }   from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
+import { ProcessingComponent } from './processing/processing.component';
+import { SearchRoutingModule } from './search-form/search-routing.module';
+import { SearchService } from './search.service';
+import { PlaceDetailComponent } from './place-detail/place-detail.component';
+import { MomentModule } from 'angular2-moment';
+import { AgmCoreModule ,GoogleMapsAPIWrapper} from '@agm/core';
+import { FavorateResultsComponent } from './favorate-results/favorate-results.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFormComponent,
+    SearchResultsComponent,
+    ProcessingComponent,
+    PlaceDetailComponent,
+    FavorateResultsComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    SearchRoutingModule,
+    HttpClientModule,
+    MomentModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyByUnSoaoYiRbHGW8NOsLfGUX_QPVWwTJ4',
+      libraries: ['places']
+     })
   ],
-  providers: [],
+  providers: [SearchService,GoogleMapsAPIWrapper],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
